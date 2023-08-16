0222fc3b91acd56bd00f1b96ab678128328a41b7
Curiel <curieldev@gmail.com>
2023-08-15 21:40:23 -0500

    Added hidden phrase generator function

--- hangman.cpp
+++ hangman.cpp
@@ -28,7 +28,7 @@ bool want_to_play_again();
 void play_hangman();
 std::string get_secret_phrase();
 void ClearScreen();
-std::string get_hidden_phrase();
+std::string make_hidden_phrase(const std::string& phrase);
 
 int main(int argc, char **argv)
 {
@@ -44,7 +44,8 @@ void play_hangman()
     const std::string secret_phrase = get_secret_phrase();
     ClearScreen();
     
-    std::string hidden_phrase = get_hidden_phrase(); // TODO: Implement get_hidden_phrase()
+    std::string  hidden_phrase = make_hidden_phrase(secret_phrase);
+
     // do
     // {
     //     draw_board()
@@ -95,43 +96,61 @@ std::string get_secret_phrase()
 
 void ClearScreen()
 {
-  HANDLE                     hStdOut;
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  DWORD                      count;
-  DWORD                      cellCount;
-  COORD                      homeCoords = {0, 0};
+    HANDLE                     hStdOut;
 
-  hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-  if (hStdOut == INVALID_HANDLE_VALUE) return;
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-  /* Get the number of cells in the current buffer */
-  if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
-  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+    // Fetch existing console mode so we correctly add a flag and not turn off others
+    DWORD mode = 0;
+    if (!GetConsoleMode(hStdOut, &mode))
+    {
+        return;
+    }
 
-  /* Fill the entire buffer with spaces */
-  if (!FillConsoleOutputCharacter(
-    hStdOut,
-    (TCHAR) ' ',
-    cellCount,
-    homeCoords,
-    &count
-    )) return;
+    // Hold original mode to restore on exit to be cooperative with other command-line apps.
+    const DWORD originalMode = mode;
+    mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-  /* Fill the entire buffer with the current colors and attributes */
-  if (!FillConsoleOutputAttribute(
-    hStdOut,
-    csbi.wAttributes,
-    cellCount,
-    homeCoords,
-    &count
-    )) return;
+    // Try to set the mode.
+    if (!SetConsoleMode(hStdOut, mode))
+    {
+        return;
+    }
 
-  /* Move the cursor home */
-  SetConsoleCursorPosition(hStdOut, homeCoords);
+    // Write the sequence for clearing the display.
+    DWORD written = 0;
+    PCWSTR sequence = L"\x1b[2J";
+    if (!WriteConsoleW(hStdOut, sequence, (DWORD)wcslen(sequence), &written, NULL))
+    {
+        // If we fail, try to restore the mode on the way out.
+        SetConsoleMode(hStdOut, originalMode);
+        return;
+    }
+
+    // To also clear the scroll back, emit L"\x1b[3J" as well.
+    // 2J only clears the visible window and 3J only clears the scroll back.
+
+    
+    // Put the cursor at its home coordinates.
+    COORD coordScreen = {0, 0};    // home for the cursor
+    SetConsoleCursorPosition(hStdOut, coordScreen);
+
+    // Restore the mode on the way out to be nice to other command-line applications.
+    SetConsoleMode(hStdOut, originalMode);
 }
 
-std::string get_hidden_phrase()
+std::string make_hidden_phrase(const std::string& phrase)
 {
     std::string hidden_phrase = "";
+    int i = 0;
+    while (phrase[i] != '\0')
+    {
+        if (isalpha(phrase[i]))
+            hidden_phrase += '_';
+        else
+            hidden_phrase += ' ';
+        i++;
+    }
+    
     return hidden_phrase;
 }

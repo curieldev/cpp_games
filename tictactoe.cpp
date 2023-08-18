9e27f4adbc433f8fa5b926956df6363cdb1dca73
Curiel <curieldev@gmail.com>
2023-08-17 22:20:34 -0500

    feat: add input range validation

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 
 bool want_to_play_again();
-template<typename T> T get_input(const std::string prompt);
+
+template<typename T> T get_value(const std::string prompt); //! Only for primitive data types
+template<typename T> T get_value(const std::string prompt, const T min, const T max); //! Only for primitive data types
 void play_tictactoe();
 void draw_board(const std::vector<char>& slots);
 
@@ -25,7 +27,7 @@ void play_tictactoe()
         int user_selection = 0;
         do
         {
-            user_selection = get_input<int>("Choose the symbol for Player 1 (o=1 or x=2): ");
+            user_selection = get_value<int>("Choose the symbol for Player 1 (o=1 or x=2): ", 0, 2);
         } while (!(user_selection == 1 || user_selection == 2));
 
         if (user_selection == 1)
@@ -45,17 +47,16 @@ void play_tictactoe()
     {
         std::cout << "Player " << player + 1 << " turn.\n";
         draw_board(slots);
-        int position = get_input<int>("Enter the desired position: ");
+        int position = get_value<int>("Enter the desired position: ", 0, 8);
 
-        // TODO: Validate values (only 0 - 8)
-        if (slots[position] == ' ')
+        if (slots[position] != ' ')
         {
-            slots[position] = player ? symbols[1] : symbols[0];
+            std::cout << "Please try again with an empty slot.\n";
+            continue;
         }
         else
         {
-            std::cout << "Please try again with a valid position.\n";
-            player = !player;
+            slots[position] = player ? symbols[1] : symbols[0];
         }
         player = !player;
     }
@@ -85,12 +86,12 @@ void draw_board(const std::vector<char>& slots)
 
 bool want_to_play_again()
 {
-    char user_input = get_input<char>("Want to play again? (y=yes, n=no): ");
+    char user_input = get_value<char>("Want to play again? (y=yes, n=no): ");
 
     return tolower(user_input) == 'y';
 }
 
-template<typename T> T get_input(const std::string prompt)
+template<typename T> T get_value(const std::string prompt)
 {
     T input = 0;
     bool failure = true;
@@ -114,4 +115,35 @@ template<typename T> T get_input(const std::string prompt)
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     return input;
+}
+
+template<typename T> T get_value(const std::string prompt, const T min, const T max) //! Only for primitive data types
+{
+    T value = 0;
+    bool failure;
+    do
+    {
+        failure = true;
+        std::cout << prompt;
+        std::cin >> value;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cout << "Input error! Please try again.\n";
+        }
+        else if (value < min || value > max)
+        {
+            std::cout << "Value is out of range: [" << min << ", " << max << "].";
+            std::cout << " Please try again.\n";
+        }
+        else
+        {
+            failure = false;
+        }
+
+    } while (failure);
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    return value;
 }
